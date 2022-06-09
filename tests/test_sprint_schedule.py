e2c592034ded93c4c233af31c18098673bee4983
Sharry Xu <sharry.xu@outlook.com>
2022-06-09 20:14:20 +0800

    Optimize the test file path.

--- tests/test_sprint_schedule.py
+++ tests/test_sprint_schedule.py
@@ -1,15 +1,18 @@
+import pathlib
+
 from jira_tool.sprint_schedule import *
 
+HERE = pathlib.Path(__file__).resolve().parent
 
 class TestSprintSchedule():
     def test_load_sprint_schedule(self):
-        schedule_filename = 'assets\\sprint_schedule.json'
+        schedule_filename = HERE.parent / 'assets/sprint_schedule.json'
         store = SprintScheduleStore()
         store.load(schedule_filename)
         assert store.total_count() > 0
 
     def test_get_priority(self):
-        schedule_filename = 'assets\\sprint_schedule.json'
+        schedule_filename = HERE.parent / 'assets/sprint_schedule.json'
         store = SprintScheduleStore()
         assert store.get_priority('R134 S2') == 0
 
--- tests/test_table_defination.py
+++ tests/test_table_defination.py
@@ -1,14 +1,18 @@
+import pathlib
+
 from jira_tool.table_defination import *
+
+HERE = pathlib.Path(__file__).resolve().parent
 
 class TestExcelColumnStore():
     def test_load(self):
-        table_defination_filename = 'assets\\table_defination.json'
+        table_defination_filename = HERE.parent / 'assets/table_defination.json'
         store = ExcelColumnStore()
         store.load(table_defination_filename)
         assert store.total_count() > 0
 
     def test_iter(self):
-        table_defination_filename = 'assets\\table_defination.json'
+        table_defination_filename = HERE.parent / 'assets/table_defination.json'
         store = ExcelColumnStore()
         store.load(table_defination_filename)
         items = []
