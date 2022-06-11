1362d78b46b7f3ccb2e513a9a98ec711519247c8
Sharry Xu <sharry.xu@outlook.com>
2022-06-11 17:02:34 +0800

    Make sure the import statements work as expected.

--- src/jira_tool/__init__.py
+++ src/jira_tool/__init__.py
@@ -1,5 +1,16 @@
 import logging
+from importlib import metadata
 
-__version__ = "0.1.6"
+from .excel_defination import *
+from .excel_operation import *
+from .milestone import *
+from .priority import *
+from .sprint_schedule import *
+from .story import *
 
-logging.basicConfig(filename='jira_tool.log', format='%(asctime)s : %(levelname)s:%(message)s')
+__version__ = metadata.version('sharry_jira_tool')
+
+del metadata
+
+logging.basicConfig(filename='jira_tool.log',
+                    format='%(asctime)s : %(levelname)s:%(message)s')
