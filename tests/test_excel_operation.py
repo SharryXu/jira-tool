ec815e242f2c88af021703f6b020fd2105c72e64
Sharry <sharry.xu@outlook.com>
2022-07-10 15:00:28 +0800

    Add more UTs.

--- tests/test_excel_operation.py
+++ tests/test_excel_operation.py
@@ -19,3 +19,27 @@ class TestExcelOperation:
         )
         assert len(columns) == 18
         assert len(stories) == 5
+
+    def test_process_excel_file(self):
+        process_excel_file(
+            HERE / "files/happy_path.xlsx",
+            HERE / "files/happy_path_sorted.xlsx",
+            sprint_schedule_config=str(HERE / "files/sprint_schedule.json"),
+            excel_defination_config=str(HERE / "files/excel_defination.json"),
+        )
+
+        excel_defination = ExcelDefination()
+        excel_defination.load_file(HERE / "files/excel_defination.json")
+        sprint_schedule = SprintScheduleStore()
+        sprint_schedule.load_file(HERE / "files/sprint_schedule.json")
+
+        _, stories = read_excel_file(
+            HERE / "files/happy_path.xlsx", excel_defination, sprint_schedule
+        )
+
+        for i in range(len(stories) - 1):
+            if (
+                stories[i]["milestone"] == stories[i + 1]["milestone"]
+                and stories[i]["criticalDefect"] == stories[i + 1]["criticalDefect"]
+            ):
+                assert stories[i] <= stories[i + 1]
--- tests/test_milestone.py
+++ tests/test_milestone.py
@@ -1,4 +1,3 @@
-
 import pathlib
 
 from jira_tool.milestone import Milestone
@@ -6,12 +5,33 @@ from jira_tool.sprint_schedule import *
 
 HERE = pathlib.Path(__file__).resolve().parent
 
-class TestMilestone():
+
+class TestMilestone:
     def test_init(self):
-        schedule_filename = HERE.parent / 'src/jira_tool/assets/sprint_schedule.json'
+        schedule_filename = HERE.parent / "src/jira_tool/assets/sprint_schedule.json"
         store = SprintScheduleStore()
         store.load_file(schedule_filename)
- 
-        milestone = Milestone('R134 S1')
+
+        milestone = Milestone("R134 S1")
         milestone.calc_priority(store)
         assert milestone.priority == 1
+
+    def test_compare(self):
+        schedule_filename = HERE.parent / "src/jira_tool/assets/sprint_schedule.json"
+        store = SprintScheduleStore()
+        store.load_file(schedule_filename)
+
+        m1 = Milestone("M109")
+        m1.calc_priority(store)
+        m2 = Milestone("M110")
+        m2.calc_priority(store)
+        m3 = Milestone("m110")
+        m3.calc_priority(store)
+        m4 = Milestone("R134 S2")
+        m4.calc_priority(store)
+        m5 = Milestone("R134 s2")
+        m5.calc_priority(store)
+        assert m1 < m2
+        assert m2 == m3
+        assert m3 == m4
+        assert m4 == m5
